8a05a2bb9a6503c20dcb638621c79cb868e51400
GalaxyBrainWave <remyzins@gmail.com>
2023-06-25 12:06:28 +0200

    page index à peu près finie

---
+++ js/burger.js
@@ -0,0 +1,23 @@
+
+let menu = document.getElementById("menu_container");
+let burger = document.getElementById("nav_burger");
+let closeButton = document.getElementById("close_button");
+
+
+function openBurger() {
+  if (menu.style.display = 'none') {
+    menu.style.display = 'flex';
+    burger.style.display = 'none';
+  }
+}
+
+function closeBUrger() {
+    if (menu.style.display = 'flex') {
+        menu.style.display = 'none';
+        burger.style.display = 'flex';
+      }
+}
+
+burger.addEventListener("click", openBurger);
+closeButton.addEventListener("click", closeBUrger);
+
